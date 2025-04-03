e814ee1600417c21808c01244785a9e9ab94a0e9
krupinin <krupinin@mdis.ru>
2025-04-03 19:47:30 +0300

    first commit

---
+++ src/main/java/ru/wish/moex_api/client/MoexApiClient.java
@@ -0,0 +1,18 @@
+package ru.wish.moex_api.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+@Component
+public class MoexApiClient {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public BigDecimal getLastPriceForTicker(String ticker) {
+        String url = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/" + ticker;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        return null;
+    }
+}
---
+++ src/test/java/ru/wish/moex_api/MoexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wish.moex_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
