ec99549b0c15af27e978e86047a847ca8fe077dc
krupinin <krupinin@mdis.ru>
2025-04-08 19:28:43 +0300

    add call getting last price

--- src/main/java/ru/wish/moex_api/service/MoexPollingService.java
+++ src/main/java/ru/wish/moex_api/service/MoexPollingService.java
@@ -1,13 +1,28 @@
 package ru.wish.moex_api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.wish.moex_api.client.MoexApiClient;
 
 @Service
 public class MoexPollingService {
     private MoexApiClient moexClient;
+    private final Logger logger = LoggerFactory.getLogger(MoexPollingService.class);
 
     public MoexPollingService(MoexApiClient moexClient){
         this.moexClient = moexClient;
+        new Thread(() -> {
+            this.run();
+        }).start();
+
+    }
+    private void run() {
+        try {
+            moexClient.getLastPriceForTicker("SBER");
+        } catch (Exception e){
+            logger.error("Failed to poll moex price", e);
+        }
     }
 }
