a9d085ec1d566fc20efa8ae566d9dc86123d0c30
krupinin <krupinin@mdis.ru>
2025-04-17 21:05:23 +0300

    add output mean price on site

--- src/main/java/ru/wish/moex_api/controller/WebController.java
+++ src/main/java/ru/wish/moex_api/controller/WebController.java
@@ -1,14 +1,24 @@
 package ru.wish.moex_api.controller;
 
 import jdk.jfr.Category;
+import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.wish.moex_api.service.DataAggregationService;
 
 @Controller
 public class WebController {
 
+    private final DataAggregationService dataAggregationService;
+
+    public WebController(DataAggregationService dataAggregationService) {
+        this.dataAggregationService = dataAggregationService;
+    }
+
     @GetMapping("/mean")
-    public String getMean() {
+    public String getMean(Model model) {
+        model.addAttribute("mean", dataAggregationService.getMean());
         return "mean";
     }
 }
