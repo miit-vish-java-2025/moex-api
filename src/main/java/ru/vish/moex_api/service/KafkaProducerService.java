5137b0a6e07fb269fb6b7a3b00307c27f734890d
Dmitry Romanov <me@wtrn.ru>
2025-05-15 19:40:59 +0300

    Manually serialize SharePriceKafkaMessage to json before sending to kafka

--- src/main/java/ru/vish/moex_api/service/KafkaProducerService.java
+++ src/main/java/ru/vish/moex_api/service/KafkaProducerService.java
@@ -1,6 +1,8 @@
 package ru.vish.moex_api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Nonnull;
+import lombok.SneakyThrows;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.vish.moex_api.kafka.model.SharePriceKafkaMessage;
@@ -11,17 +13,21 @@ import java.time.Instant;
 @Service
 public class KafkaProducerService {
 
-    private final KafkaTemplate<String, SharePriceKafkaMessage> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public KafkaProducerService(KafkaTemplate<String, SharePriceKafkaMessage> kafkaTemplate) {
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
+    @SneakyThrows
     public void publish(@Nonnull BigDecimal price) {
         SharePriceKafkaMessage message = SharePriceKafkaMessage.builder()
                 .timestamp(Instant.now())
                 .price(price)
                 .build();
-        kafkaTemplate.send("share-prices", message);
+        String messageString = objectMapper.writeValueAsString(message);
+        kafkaTemplate.send("share-prices", messageString);
     }
 }
