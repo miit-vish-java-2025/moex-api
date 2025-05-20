5a0ed614402aa7fd2b84900f3257ad0f61e38172
BND86 <solodko24@gmail.com>
2025-05-20 19:21:38 +0300

    Kafka delete

--- src/main/java/ru/vish/moex_api/configuration/BeanConfiguration.java
+++ src/main/java/ru/vish/moex_api/configuration/BeanConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import ru.vish.moex_api.client.MoexApiClient;
 import ru.vish.moex_api.repository.PriceHistoryRepository;
 import ru.vish.moex_api.service.DataAggregationService;
-import ru.vish.moex_api.service.KafkaProducerService;
 import ru.vish.moex_api.service.MoexPollingService;
 
 @Configuration
@@ -16,7 +15,7 @@ public class BeanConfiguration {
     }
 
     @Bean
-    public MoexPollingService moexPollingService(MoexApiClient moexClient, DataAggregationService aggService, KafkaProducerService kafkaProducerService) {
-        return new MoexPollingService(moexClient, aggService, kafkaProducerService);
+    public MoexPollingService moexPollingService(MoexApiClient moexClient, DataAggregationService aggService) {
+        return new MoexPollingService(moexClient, aggService);
     }
 }
